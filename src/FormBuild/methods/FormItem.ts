3bdfa7fed703b8880c9d6f697ebeddfa423c021e
yajun.xu <yajun.xu@bthome.com>
2023-06-16 18:22:36 +0800

    feat(save): save

---
+++ src/FormBuild/methods/FormItem.ts
@@ -0,0 +1,5 @@
+import { FormItemOption } from "./types.d";
+
+export default class FormItem {
+  constructor(option: FormItemOption) {}
+}
--- src/FormBuild/methods/types.d.ts
+++ src/FormBuild/methods/types.d.ts
@@ -8,3 +8,40 @@ export interface FormModuleOption {
   labelWidth: string; // label宽度
   disabled: boolean; // 是否禁用
 }
+
+export type FormItemType =
+  | "input"
+  | "textarea"
+  | "password"
+  | "count"
+  | "select"
+  | "cascader"
+  | "radio"
+  | "checkbox"
+  | "switch"
+  | "slider"
+  | "time"
+  | "times"
+  | "date"
+  | "dates"
+  | "rate"
+  | "upload"
+  | "block"
+  | "button";
+
+export type DataType = "number" | "datetime" | "string";
+
+export interface FormItemOption {
+  type: FormItemType; // 组件类型
+  name: string; // 字段名称
+  dataType: DataType; // 数据类型
+  maxLength: number; // 最大输入数量
+  primaryKey: boolean; // 是否是主键
+  autoIncrement: boolean; // 是否自增
+  connectTable: string; // 关联表
+  connectCol: string; // 关联字段
+
+  defaultValue: string | number | Date; // 默认值
+  require: boolean; // 是否必须
+  placeholder: string; // 占位提示符
+}
