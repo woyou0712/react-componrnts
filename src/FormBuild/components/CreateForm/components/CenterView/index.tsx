1e78999b67ba2b6c3fdca31236b5a301038ad71c
yajun.xu <yajun.xu@bthome.com>
2023-06-19 19:50:06 +0800

    feat(save): save

--- src/FormBuild/components/CreateForm/components/CenterView/index.tsx
+++ src/FormBuild/components/CreateForm/components/CenterView/index.tsx
@@ -3,8 +3,18 @@ import React from "react";
 import PropTypes from "prop-types";
 
 import "./index.less";
+import { useDrop } from "react-dnd";
 
 function CenterView() {
+  useDrop(() => ({
+    type: "box",
+    accept: "box",
+    drop(item, monitor) {
+      const dropResult = monitor.getDropResult();
+      console.log(dropResult);
+    },
+    collect: (monitor) => ({ canDrop: monitor.canDrop() }),
+  }));
   return (
     <div className="create-form-center-view">
       <span>CenterView Component</span>
