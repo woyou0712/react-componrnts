e9a9d7f204a12584766a754323c9c6a5f7795c61
yajun.xu <yajun.xu@bthome.com>
2023-08-16 17:06:45 +0800

    feat(save): save

--- src/BPMNView/components/Tools/index.ts
+++ src/BPMNView/components/Tools/index.ts
@@ -1,6 +1,7 @@
 import SelfPalette from "./SelfPalette";
 import SelfPaletteProvider from "./SelfPaletteProvider";
 import SelfContextPadProvider from "./SelfContextPadProvider";
+
 export default {
   __init__: ["palette", "paletteProvider", "contextPadProvider"],
   palette: ["type", SelfPalette],
