a3c309bb5547d3f70e7b24ecd11e2f561d8df558
yajun.xu <yajun.xu@bthome.com>
2023-08-21 11:03:19 +0800

    feat(save): 自定义节点

--- src/BPMNView/components/Tools/SelfContextPadProvider.ts
+++ src/BPMNView/components/Tools/SelfContextPadProvider.ts
@@ -15,7 +15,7 @@ import {
   Rules,
   Shape,
 } from "../../methods/types";
-import { createAction } from "./utils";
+import { appendAction, createAction } from "./utils";
 
 export default class SelfContextPadProvider extends ContextPadProvider {
   private config: ContextPadConfig;
@@ -80,14 +80,42 @@ export default class SelfContextPadProvider extends ContextPadProvider {
     delete newData["replace"]; // 删除扳手（转换类型）工具
     delete newData["append.end-event"]; // 删除结束节点
     delete newData["append.intermediate-event"]; // 删除中间事件节点
-    delete newData["append.append-task"]; // 删除任务节点
+    delete newData["append.append-task"]; // 删除空白的任务节点
+    delete newData["append.text-annotation"]; // 删除文本注释节点
     // 不是结束节点，在节点工具上添加任务和抄送节点
     if (shape.type !== "bpmn:EndEvent") {
-      // newData["append.append-service-task"]
-      // newData["append.append-service-task"]
+      newData["append.append-user-task"] = appendAction(
+        "bpmn:UserTask",
+        "model",
+        "bpmn-icon-user-task",
+        "添加任务节点",
+        this.translate,
+        this.elementFactory,
+        this.create,
+        this.injector
+      );
+      newData["append.append-service-task"] = appendAction(
+        "bpmn:ServiceTask",
+        "model",
+        "bpmn-icon-service-task",
+        "添加抄送节点",
+        this.translate,
+        this.elementFactory,
+        this.create,
+        this.injector
+      );
     }
     if (newData["append.text-annotation"]) {
       newData["append.text-annotation"].group = "connect";
+    }
+    if (newData["append.gateway"]) {
+      newData["append.gateway"].title = "添加条件节点";
+    }
+    if (newData["connect"]) {
+      newData["connect"].title = "连接其他节点";
+    }
+    if (newData["delete"]) {
+      newData["delete"].title = "删除当前节点";
     }
     console.log("节点工具", newData);
     return newData;
--- src/BPMNView/components/Tools/utils.ts
+++ src/BPMNView/components/Tools/utils.ts
@@ -1,6 +1,12 @@
 import { getDi } from "bpmn-js/lib/util/ModelUtil";
 import { assign } from "min-dash";
-import { Create, ElementFactory, PaletteEntry, Translate } from "../../methods/types";
+import {
+  Create,
+  ElementFactory,
+  Injector,
+  PaletteEntry,
+  Translate,
+} from "../../methods/types";
 
 /**
  * 创建节点类型对象
@@ -33,4 +39,48 @@ export function createAction(
     action: { dragstart: createListener, click: createListener },
   };
   return result;
+}
+/**
+ * 添加节点工具
+ */
+export function appendAction(
+  type: string,
+  group: string,
+  className: string,
+  title: string,
+  translate: Translate,
+  elementFactory: ElementFactory,
+  create: Create,
+  injector: Injector,
+  options?: any
+): any {
+  if (typeof title !== "string") {
+    options = title;
+    title = translate("Append {type}", { type: type.replace(/^bpmn:/, "") });
+  }
+
+  function appendStart(event, element) {
+    var shape = elementFactory.createShape(assign({ type: type }, options));
+    create.start(event, shape, {
+      source: element,
+    });
+  }
+  const autoPlace: any = injector.get("autoPlace", false);
+  var append = autoPlace
+    ? function (event, element) {
+        var shape = elementFactory.createShape(assign({ type: type }, options));
+
+        autoPlace.append(element, shape);
+      }
+    : appendStart;
+
+  return {
+    group,
+    className: className,
+    title: title,
+    action: {
+      dragstart: appendStart,
+      click: append,
+    },
+  };
 }
